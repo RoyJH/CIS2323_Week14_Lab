034241499b2f91f1476a9c138592a15d26d03adc
royjh <royjh@okstate.edu>
2018-12-05 20:15:12 -0600

    added row.xml, mainActivity code, idNames

--- app/src/main/java/com/example/svc_okc_bt228/cis2323_week14_lab/MainActivity.java
+++ app/src/main/java/com/example/svc_okc_bt228/cis2323_week14_lab/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.svc_okc_bt228.cis2323_week14_lab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper db = new dbHelper(context this);
+        ListView lstTask = findViewById(R.id.lstTask);
+
+        ArrayList<String> taskList = db.getTaskList();
+        ArrayAdapter<String> mArrayAdaptor = new ArrayAdapter<String>(this, R.layout.row, R.id.task_title, taskList);
     }
 }
